fc3f9b4095362b7a42ff989df22a016b360b6e33
umesh-air <uumeshayer@gmail.com>
2026-09-12 15:30:51 +0545

    variables code

---
+++ Variables.py
@@ -0,0 +1,5 @@
+#We can make a variable by assigning a value to it. For example, we can create a variable called "name" and assign it the value "John":
+
+name = "John"
+print(name)  # This will output: John
+
